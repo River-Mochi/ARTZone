63b8897829eeca454d00b6e20e06f59e17eb15db
LuxStice <costahpedro@gmail.com>
2025-02-22 02:49:21 +0000

    Fixed logging error
    Signed-off-by: LuxStice <costahpedro@gmail.com>

--- src/AdvancedRoadTools/Logging/LogART.cs
+++ src/AdvancedRoadTools/Logging/LogART.cs
@@ -4,14 +4,14 @@ namespace AdvancedRoadTools.Logging;
 
 public static class LogART
 {
-    public static ILog logger = LogManager.GetLogger($"[{nameof(AdvancedRoadTools)}")
+    public static ILog logger = LogManager.GetLogger($"{nameof(AdvancedRoadTools)}")
         .SetShowsErrorsInUI(false);
 
-    public static void Info(string content) => logger.Info(" Info] " + content);
+    public static void Info(string content) => logger.Info(content);
 
-    public static void Debug(string content) => logger.Debug(" Debug] " + content);
+    public static void Debug(string content) => logger.Debug(content);
 
-    public static void Warn(string content) => logger.Warn(" Warn] " + content);
+    public static void Warn(string content) => logger.Warn(content);
 
-    public static void Error(string content) => logger.Error(" Error] " + content);
+    public static void Error(string content) => logger.Error(content);
 }
